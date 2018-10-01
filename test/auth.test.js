7f3a6e3458b2e087938c9f6deac8bba78888225e
kendrajmoore <kendra.j.moore3443@gmail.com>
2018-10-01 19:56:23 -0400

    add pseudo for tests

--- test/auth.test.js
+++ test/auth.test.js
@@ -9,3 +9,10 @@ var agent = chai.request.agent(server);
 var User = require("../models/user");
 
 describe("User", function() {});
+
+// Return 401 if user not logged
+//Return 400 if id not provided
+//Return 400 if no character id is provided
+//return 404 if no infor for customer
+// return 200 if valid request
+//
