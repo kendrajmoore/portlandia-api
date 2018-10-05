4cdd18934c4fb21084cc8a170a3e9246ffc08609
kendrajmoore <kendra.j.moore3443@gmail.com>
2018-10-05 18:18:48 -0400

    revise tests

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -1,18 +1,44 @@
-var chai = require("chai");
-var chaiHttp = require("chai-http");
-var server = require("../app");
-var should = chai.should();
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = require("../server");
+const should = chai.should();
 chai.use(chaiHttp);
 
-var agent = chai.request.agent(server);
+const agent = chai.request.agent(server);
 
-var User = require("../models/user");
+const User = require("../models/user");
 
 describe("User", function() {});
+// signup
+it("should not be able to login if they have not registered", done => {
+    agent
+        .post("/portlandia/user/login", {
+            email: "wrong@wrong.com",
+            password: "nope"
+        })
+        .end(function(err, res) {
+            res.status.should.be.equal(401);
+            done();
+        });
+});
 
-// Return 401 if user not logged
-//Return 400 if id not provided
-//Return 400 if no character id is provided
-//return 404 if no infor for customer
-// return 200 if valid request
-//
+// logout
+it("should be able to logout", done => {
+    agent.get("/portlandia/user/logout").end(function(err, res) {
+        res.should.have.status(200);
+        res.should.not.have.cookie("nToken");
+        done();
+    });
+});
+
+// login
+it("should be able to login", done => {
+    agent
+        .post("/portlandia/user/login")
+        .send({ email: "username", password: "password" })
+        .end(function(err, res) {
+            res.should.have.status(200);
+            res.should.have.cookie("nToken");
+            done();
+        });
+});
